18b964a5647a2313461f109f79a40f0a044d5de8
Jacó Miranda <jaco-miranda@hotmail.com>
2019-10-17 21:59:04 -0300

    Finalização aula sobre variáveis e funções básicas no JS.

---
+++ js/main.js
@@ -0,0 +1,90 @@
+/*var d = new Date();
+//alert(d);
+//alert(d.getMonth() + 1);
+alert(d.getMinutes());
+*/
+
+/*var count;
+ for (count = 0; count < 5; count++) {
+     alert(count);
+     console.log(count);
+ };
+*/
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+};
+*/
+
+/*var idade = prompt("Qual sua idade?");
+//var idade = 16;
+if (idade >= 18) {
+    alert("Maior de Idade")
+} else {
+    alert("Menor de idade")
+}
+*/
+/*var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "Manga", cor: "amarela" }];
+console.log(frutas);
+alert(frutas[1].cor);
+*/
+/*
+var fruta = { nome: "maçã", cor: "vermelha" };
+console.log(fruta);
+*/
+//var lista = ["maçã", "pera", "morango"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" | "));
+
+
+/*var nome = "Jaco Miranda";
+var idade = "43";
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade);
+//alert(idade + idade);
+
+console.log(nome);
+console.log(idade + " Jacó Miranda");
+console.log(frase.toLowerCase());
+*/ //
+
+//Função
+/*
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar() {
+    // window.open("http://www.google.com.br")
+    window.location.href = "http://www.google.com.br";
+}
+
+function trocar(elemento) {
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passsar o mouse";
+    elemento.innerHTML = " Obrigado por passar o mouse";
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+*/
+
+function load() {
+    alert("Página Carregada")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+}
